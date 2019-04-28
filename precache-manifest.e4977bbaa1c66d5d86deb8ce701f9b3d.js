191071752a9ebd9a1d4b797a085fb0b97bfa7b0f
Denis Iogansen <pofigizm@gmail.com>
2019-04-28 21:20:15 +0300

    Updates

---
+++ precache-manifest.e4977bbaa1c66d5d86deb8ce701f9b3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a86bc0d14e6818a25966ba836fd8c8",
+    "url": "/sch2/index.html"
+  },
+  {
+    "revision": "ecb8f12cdbc137bdea80",
+    "url": "/sch2/static/css/main.25dd7899.chunk.css"
+  },
+  {
+    "revision": "6dfd36fe3d974b3e2ac0",
+    "url": "/sch2/static/js/2.9d843c02.chunk.js"
+  },
+  {
+    "revision": "ecb8f12cdbc137bdea80",
+    "url": "/sch2/static/js/main.1815a54c.chunk.js"
+  },
+  {
+    "revision": "72839c3ec93d8a9012f8",
+    "url": "/sch2/static/js/runtime~main.8aa2d155.js"
+  }
+]);
